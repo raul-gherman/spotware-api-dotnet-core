6a5dc7afa4c0e008b531732c95d0c557957a158d
Raul <raul.gherman@yandex.com>
2020-01-20 13:45:10 +0200

    Update API definition to 6.5

---
+++ src/messages/responses/Refresh_Token_Res.cs
@@ -0,0 +1,27 @@
+﻿using ProtoBuf;
+
+namespace spotware
+{
+    public partial class Client
+    {
+        private void Process_Refresh_Token_Res()
+        {
+            ProtoOARefreshTokenRes args = Serializer.Deserialize<ProtoOARefreshTokenRes>(_processorMemoryStream);
+
+            _log.Info($"RefreshTokenRes: | "                  +
+                      $"tokenType: {args.tokenType}| "        +
+                      $"expiresIn: {args.expiresIn} | "       +
+                      $"accessToken: {args.accessToken} | "   +
+                      $"refreshToken: {args.refreshToken} | " +
+                      $"expiresIn: {args.expiresIn}");
+
+            // TODO: restart the entire flow, from authorization
+
+            OnRefreshTokenRes_Received?.Invoke(args);
+        }
+
+        public event RefreshTokenRessReceived OnRefreshTokenRes_Received;
+
+        public delegate void RefreshTokenRessReceived(ProtoOARefreshTokenRes args);
+    }
+}
