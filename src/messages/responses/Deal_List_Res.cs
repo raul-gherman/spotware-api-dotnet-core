da1847416c1ce622e380f91b0f15d6393fe26599
Raul <raul.gherman@yandex.com>
2020-04-25 08:37:53 +0300

    Get symbolName by symbolID in Deal List Res

--- src/messages/responses/Deal_List_Res.cs
+++ src/messages/responses/Deal_List_Res.cs
@@ -10,28 +10,28 @@ namespace spotware
 
             foreach (ProtoOADeal deal in args.Deals)
             {
-                string item = $"dealId: {deal.dealId}; "                                                                                     +
-                              $"dealStatus: {deal.dealStatus}; "                                                                             +
-                              $"orderId: {deal.orderId}; "                                                                                   +
-                              $"positionId: {deal.positionId}; "                                                                             +
-                              $"symbolId: {deal.symbolId} ({TradingAccounts[args.ctidTraderAccountId].TradingSymbols[position.tradeData.symbolId].LightSymbol.symbolName}); " +
-                              $"tradeSide: {deal.tradeSide}; "                                                                               +
-                              $"executionPrice: {deal.executionPrice}; "                                                                     +
-                              $"Volume: {deal.Volume}; "                                                                                     +
-                              $"filledVolume: {deal.filledVolume}; "                                                                         +
-                              $"marginRate: {deal.marginRate}; "                                                                             +
-                              $"Commission: {deal.Commission}; "                                                                             +
-                              $"createTimestamp: {deal.createTimestamp} ({EpochToString(deal.createTimestamp)}; "                            +
-                              $"executionTimestamp: {deal.executionTimestamp} ({EpochToString(deal.executionTimestamp)}; "                   +
-                              $"closePositionDetail.Balance: {deal.closePositionDetail.Balance}; "                                           +
-                              $"closePositionDetail.balanceVersion: {deal.closePositionDetail.balanceVersion}; "                             +
-                              $"closePositionDetail.closedVolume: {deal.closePositionDetail.closedVolume}; "                                 +
-                              $"closePositionDetail.entryPrice: {deal.closePositionDetail.entryPrice}; "                                     +
-                              $"closePositionDetail.quoteToDepositConversionRate: {deal.closePositionDetail.quoteToDepositConversionRate}; " +
-                              $"closePositionDetail.Swap: {deal.closePositionDetail.Swap}; "                                                 +
-                              $"closePositionDetail.Commission: {deal.closePositionDetail.Commission}; "                                     +
-                              $"closePositionDetail.grossProfit: {deal.closePositionDetail.grossProfit}; "                                   +
-                              $"utcLastUpdateTimestamp: {deal.utcLastUpdateTimestamp} ({EpochToString(deal.utcLastUpdateTimestamp)}; "       +
+                string item = $"dealId: {deal.dealId}; "                                                                                                        +
+                              $"dealStatus: {deal.dealStatus}; "                                                                                                +
+                              $"orderId: {deal.orderId}; "                                                                                                      +
+                              $"positionId: {deal.positionId}; "                                                                                                +
+                              $"symbolId: {deal.symbolId} ({TradingAccounts[args.ctidTraderAccountId].TradingSymbols[deal.symbolId].LightSymbol.symbolName}); " +
+                              $"tradeSide: {deal.tradeSide}; "                                                                                                  +
+                              $"executionPrice: {deal.executionPrice}; "                                                                                        +
+                              $"Volume: {deal.Volume}; "                                                                                                        +
+                              $"filledVolume: {deal.filledVolume}; "                                                                                            +
+                              $"marginRate: {deal.marginRate}; "                                                                                                +
+                              $"Commission: {deal.Commission}; "                                                                                                +
+                              $"createTimestamp: {deal.createTimestamp} ({EpochToString(deal.createTimestamp)}; "                                               +
+                              $"executionTimestamp: {deal.executionTimestamp} ({EpochToString(deal.executionTimestamp)}; "                                      +
+                              $"closePositionDetail.Balance: {deal.closePositionDetail.Balance}; "                                                              +
+                              $"closePositionDetail.balanceVersion: {deal.closePositionDetail.balanceVersion}; "                                                +
+                              $"closePositionDetail.closedVolume: {deal.closePositionDetail.closedVolume}; "                                                    +
+                              $"closePositionDetail.entryPrice: {deal.closePositionDetail.entryPrice}; "                                                        +
+                              $"closePositionDetail.quoteToDepositConversionRate: {deal.closePositionDetail.quoteToDepositConversionRate}; "                    +
+                              $"closePositionDetail.Swap: {deal.closePositionDetail.Swap}; "                                                                    +
+                              $"closePositionDetail.Commission: {deal.closePositionDetail.Commission}; "                                                        +
+                              $"closePositionDetail.grossProfit: {deal.closePositionDetail.grossProfit}; "                                                      +
+                              $"utcLastUpdateTimestamp: {deal.utcLastUpdateTimestamp} ({EpochToString(deal.utcLastUpdateTimestamp)}; "                          +
                               $"baseToUsdConversionRate: {deal.baseToUsdConversionRate}";
 
                 Log.Info("ProtoOADealListRes:: "                              +
